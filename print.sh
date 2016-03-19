854b09519e3a568be556487cae7c0f31c991b5f4
crazymind <zgbkny@163.com>
2016-03-18 22:19:49 -0700

    first

---
+++ print.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ulimit -n 10000
+printf "%-5s %-10s %-4s\n" No Name Mark
+printf "%-5s %-10s %-4s\n" 1 Sarath 80.2352
+printf "%-5s %-10s %-4s\n" 2 Jams 12.2452
+printf "%-5s %-10s %-4s\n" 3 Jeff 7.3456456
